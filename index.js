0cfadc3324bcff7771125ecd33783d083ddc595a
Kevin Kim <kevink856@gmail.com>
2023-06-02 13:57:10 -0700

    Fix deprecated header format

--- index.js
+++ index.js
@@ -10,11 +10,10 @@ const app = express();
 app.use(express.json());
 app.use(cors({ origin: "https://kevink856.github.io" }));
 
-app.use(function(req, res, next) {
+app.use(function(req, res) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.next();
+    res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD");
+    res.header("Access-Control-Allow-Headers", "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,Origin,X-Requested-With,Accept");
 });
 
 // Load the /posts routes

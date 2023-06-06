26d393d73a3fb15d28234c5767d5d1582557fca5
Kevin Kim <kevink856@gmail.com>
2023-06-05 22:14:49 -0700

    Delete redundant CORS headers
    Added headers directly to API Gateway

--- index.js
+++ index.js
@@ -8,13 +8,7 @@ require("dotenv").config();
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: "https://kevink856.github.io" }));
-
-app.use(function(req, res) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD");
-    res.header("Access-Control-Allow-Headers", "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,Origin,X-Requested-With,Accept");
-});
+app.use(cors());
 
 // Load the /posts routes
 app.use("/top", top);
